bacc6608096c313c851c659772166aca71b36451
Solomon <elmeriki@gmail.com>
2024-04-25 07:47:55 +0200

    update with url

--- Swiftsend/urls.py
+++ Swiftsend/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('swift/', admin.site.urls),
     path('', include('swiftauth.urls')),
     path('', include('customer.urls'))
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
